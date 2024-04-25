3c5c4879283d312f31bd1013d9731e0a40874b17
Arnab Deep Nath <86016150+ArnabDeepNath@users.noreply.github.com>
2024-04-25 18:57:00 +0530

    DG Project
    - Backend Tweaks

--- js/index.js
+++ js/index.js
@@ -2,6 +2,42 @@ const startButton = document.querySelector('.product_btn');
 const timerContainer = document.querySelector('.timer');
 const message = document.getElementById('message');
 
+startButton.addEventListener('click', async () => {
+  const wish = document.querySelector('input[name="wish"]').value;
+  const delayTime = parseInt(
+    document.querySelector('input[name="delayTime"]').value,
+  );
+
+  try {
+    // Get the token from localStorage
+    const token = localStorage.getItem('token');
+
+    // Send request to start the timer
+    const response = await fetch(
+      'https://dg-backend-9135cdee7c9e.herokuapp.com/start-timer/start-timer',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`, // Include the token in the request headers
+        },
+        body: JSON.stringify({ wish, duration: delayTime }), // Include the wish in the request body
+      },
+    );
+
+    if (response.ok) {
+      // Timer started successfully, display countdown
+      displayTimer(delayTime);
+      message.textContent = 'Timer started successfully.';
+    } else {
+      message.textContent = 'Failed to start timer.';
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    message.textContent = 'An error occurred. Please try again.';
+  }
+});
+
 function displayTimer(duration) {
   let endTime = localStorage.getItem('endTime');
   const currentTime = Date.now();
@@ -41,40 +77,5 @@ window.addEventListener('load', () => {
   const duration = parseInt(localStorage.getItem('duration'), 10);
   if (duration) {
     displayTimer(duration);
-  }
-});
-
-// Event listener for the Start button
-startButton.addEventListener('click', async () => {
-  const wish = document.querySelector('input[name="wish"]').value;
-  const duration = parseInt(
-    document.querySelector('input[name="delayTime"]').value,
-    10,
-  );
-
-  try {
-    // Send request to start the timer
-    const response = await fetch(
-      'https://dg-backend-9135cdee7c9e.herokuapp.com/start-timer/start-timer',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({ duration }),
-      },
-    );
-
-    if (response.ok) {
-      localStorage.setItem('duration', duration);
-      displayTimer(duration);
-      message.textContent = 'Timer started successfully.';
-    } else {
-      message.textContent = 'Failed to start timer.';
-    }
-  } catch (error) {
-    console.error('Error:', error);
-    message.textContent = 'An error occurred. Please try again.';
   }
 });
