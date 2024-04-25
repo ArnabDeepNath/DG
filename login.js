c3a49910c667f35997f015a4a6e82a5fc65718c7
Arnab Deep Nath <86016150+ArnabDeepNath@users.noreply.github.com>
2024-04-25 14:50:18 +0530

    DG Project
    - Added Login Functionality

--- login.js
+++ login.js
@@ -1,32 +1,26 @@
-const form = document.getElementById('registerForm');
+const form = document.getElementById('loginForm');
 const message = document.getElementById('message');
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
 
   const formData = new FormData(form);
-  const username = formData.get('username');
   const email = formData.get('email');
   const password = formData.get('password');
 
   try {
-    const response = await fetch(
-      'https://dg-backend-9135cdee7c9e.herokuapp.com/auth/register',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          username: username,
-          email: email,
-          password: password,
-        }),
+    const response = await fetch('/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
       },
-    );
+      body: JSON.stringify({ email, password }),
+    });
     const data = await response.json();
     if (response.ok) {
-      message.textContent = 'Registration successful. Please log in.';
+      // Store the token in local storage or session storage
+      localStorage.setItem('token', data.token);
+      message.textContent = 'Login successful.';
       form.reset();
     } else {
       message.textContent = data.message;
